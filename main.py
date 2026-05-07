ec26053513d05c3e89afb482c8f1d4352269f8cd
Anflouss <lhossaynanfloss1992@gmail.com>
2026-05-07 19:55:49 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+import os
+from telegram import Update
+from telegram.ext import Application, CommandHandler, MessageHandler, filters
+
+TOKEN = os.environ.get('TELEGRAM_TOKEN')
+
+async def start(update, context):
+    await update.message.reply_text('السلام عليكم أ ستوديو! البوت خدام 🚀')
+
+def main():
+    app = Application.builder().token(TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    app.run_polling()
+
+if __name__ == '__main__':
+    main()
